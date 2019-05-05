9bc7e51979d0e0e5b5ac0f2ba9ff8286b5a5ead7
shashidharjavaji <shashidharjavaji@gmail.com>
2019-05-05 22:08:34 +0530

    reverse of an array

--- reversearray_shashidhar.c
+++ reversearray_shashidhar.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-int main()
-{
-   char stringOne[1000], reverseString[1000];
-   int begin, end, count = 0;
  
-   printf("Enter the String to be reversed\n");
-   gets(stringOne);
-   while (stringOne[count] != '\0')
-      count++;
-   end = count - 1;
-   for (begin = 0; begin < count; begin++) {
-      reverseString[begin] = stringOne[end];
-      end--;
-   }
-
-   reverseString[begin] = '\0';
- 
-   printf("%s\n", r);
-
-   return 0;
+int main() {
+  int array[100], length, initial, temp, end;
+  printf("enter the length of the array\n");
+  scanf("%d", &length);
+  end = length- 1;
+  printf("enter the values\n");
+  for (initial = 0; initial < length; initial++) {
+    scanf("%d", &array[initial]);
+  }
+  for (intial = 0; initial < length/2; initial++) {
+    temp        = array[initial];
+    array[initial]   = array[end];
+    array[end] = temp;
+    end--;
+  } 
+  printf("Reversed array elements are:\n");
+  for (intial = 0; initial < length; initial++) {
+    printf("%d\n", array[initial]);
+  }
+  return 0;
 }
