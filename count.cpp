ff1c004fa51019091a88abb88a08d0f31c91716b
shashidharjavaji <shashidharjavaji@gmail.com>
2019-05-09 18:19:33 +0530

    count program

---
+++ count.cpp
@@ -0,0 +1,28 @@
+/*
+OVERVIEW:  Given 2 integers (num1, num2), write a function that returns the count of numbers between the num1..num2 that are divisible by num1.
+		E.g.: count(3, 15) returns 5 (3, 6, 9, 12, 15).
+		Note : You need to consider Inclusive range
+INPUTS:  Two numbers num1, num2.
+		num1, num2 >= 0.
+
+OUTPUT: Return the count of numbers between the num1..num2 that are divisible by num1.
+
+ERROR CASES: Return -1 in error cases.
+
+NOTES:
+*/
+int count(int num1, int num2) {
+	int count = 0;
+	if (num1 <= 0 )
+	{
+		return -1;
+	}
+	for (int number=num1; number<= num2;number++)
+	{
+		if (number%num1 == 0)
+		{
+			count++;
+		}
+	}
+		return count;
+}
